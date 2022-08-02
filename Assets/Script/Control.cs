c4918822cd7585512e2abfeb1b9fd1375497995e
BaeGWoo <gwbae4211@naver.com>
2022-08-02 21:46:01 +0900

    Control & Particle

--- Assets/Script/Control.cs
+++ Assets/Script/Control.cs
@@ -4,43 +4,91 @@ using UnityEngine;
 
 public class Control : MonoBehaviour
 {
-   
-
-    //마우스 회전 속도
-    [SerializeField] float angleSpeed = 3.5f;
+    //카메라 x축의 회전속도
+    [SerializeField] float Xspeed=5.0f;
+    //카메라 y축의 회전속도
+    [SerializeField] float Yspeed = 3.0f;
 
     //내부에 사용할 x축 회전량을 따로 정의
-    [SerializeField] float xRotation;
+    [SerializeField] float eulerAngleX;
+    [SerializeField] float eulerAngleY;
 
-    
 
-   
+    [SerializeField] float moveSpeed;//이동 속도
+    private Vector3 moveForce;//이동하는 힘
+
+    float gravity = -9.81f;
+
+
+    private CharacterController characterControl;
+
+    public Transform cameraTransform;
+    private ParticleSystem particle;
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        characterControl = GetComponent<CharacterController>();
+
+        particle = GetComponentInChildren<ParticleSystem>();
+    }
+
+
     void Update()
     {
-        MouseRotation();
 
+        UpdateRotate(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 
+        if(Input.GetButtonDown("Fire1"))
+        {
+            particle.Play();
+        }
+
+        if (!characterControl.isGrounded)
+        {
+            moveForce.y += gravity * Time.deltaTime;
+        }
+        else
+        {
+            moveForce.y = 0.1f;
+        }
+        MoveTo(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
+        characterControl.Move(moveForce * Time.deltaTime);
 
     }
 
 
-    //마우스 움직임에 따라 카메라를 회전 시키는 함수
-    private void MouseRotation()
+    public void MoveTo(Vector3 direction)
     {
-        //좌우로 움직인 마우스의 이동량 * 속도에 따라 카메라가 좌우로 회전하는 값
-        float ySize = Input.GetAxis("Mouse X") * angleSpeed;
+        //이동 방향=캐릭터의 회전 값* 방향 값
+        direction = transform.rotation * new Vector3(direction.x, 0, direction.z);
 
-        //현재 y축 회전값에 더한 새로운 회전 각도 계산
-        float y = transform.eulerAngles.y + ySize;
-
-        // 위아래로 움직이는 마우스의 이동량 * 속도에 따라 카메라가 위아래로 회전하는 값
-        float xSize = -Input.GetAxis("Mouse Y") * angleSpeed;
-
-        //-45는 하늘 방향, 80은 바닥 방향
-        xRotation = Mathf.Clamp(xRotation + xSize, -45 , 80);
-
-        transform.eulerAngles = new Vector3(xRotation, y, 0);
+        //이동하는 힘=이동 방향*속도
+        moveForce = new Vector3(direction.x * moveSpeed, moveForce.y, direction.z * moveSpeed);
     }
 
+    public void UpdateRotate(float mouseX, float mouseY)
+    {
+        eulerAngleY += mouseX * Yspeed;//마우스 좌우 이동으로 카메라 y축 회전
+                                       //마우스를 아래로 내리면 -로 음수가 되는데 오브젝트의 x축이 +방향으로 회전해야 아래를 보기 때문에 -로 설정
+        eulerAngleX -= mouseY * Xspeed;//마우스 위아래 이동으로 카메라 x축 회전
+
+        eulerAngleX = ClampAngle(eulerAngleX, -80, 50);
+
+        transform.rotation = Quaternion.Euler(transform.rotation.x,eulerAngleY,0);
+
+        cameraTransform.rotation = Quaternion.Euler(eulerAngleX, transform.eulerAngles.y, 0);
+    }
+
+    public float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360)
+            angle += 360;
+
+        if (angle > 360)
+            angle -= 360;
+
+        return Mathf.Clamp(angle, min, max);
+    }
 
 }
